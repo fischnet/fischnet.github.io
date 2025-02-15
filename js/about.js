b68b29c6ee23211585c95cabaf5c4f73730e46f7
fischnet <madeinheaven3145@gmail.com>
2025-02-16 00:08:27 +0200

    no dark mode :(

--- js/about.js
+++
@@ -1,9 +0,0 @@
-function toggleTheme() {
-    document.body.classList.toggle("dark-mode");
-    const icon = document.querySelector(".theme-toggle i");
-    if (document.body.classList.contains("dark-mode")) {
-        icon.classList.replace("fi-br-moon-stars", "fi-br-sun");
-    } else {
-        icon.classList.replace("fi-br-sun", "fi-br-moon-stars");
-    }
-}
